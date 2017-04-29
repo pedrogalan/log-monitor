07685c6349fbe57b5ed5f6e4f14886e017ef15ba
Pedro Galan <pedro.galan@piksel.com>
2017-04-29 20:44:12 +0100

    Config class to handle configuration

---
+++ __init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+from config.Config import Config
+
+print(Config.get("mail.receiver"))
---
+++ config/Config.py
@@ -0,0 +1,36 @@
+from ConfigParser import ConfigParser
+from os.path import expanduser
+
+def getConfigFileName():
+    return expanduser("~") + '/.log-monitor'
+
+def loadProperties():
+    config = ConfigParser()
+    try:
+        with open(getConfigFileName()) as f:
+            config.readfp(f)
+    except IOError:
+        createDefaultProperties()
+        config.read(getConfigFileName())
+    return config
+
+def createDefaultProperties():
+    file = open(getConfigFileName(), "w")
+    file.write("[all]\n\n")
+    file.write("mail.smtp.host=change-me\n")
+    file.write("mail.smtp.username=change-me\n")
+    file.write("mail.smtp.password=change-me\n")
+    file.write("mail.receiver=Mr. Change Me <change@me.com>\n")
+    file.write("mail.sender=Mr. Change Me <change@me.com>\n\n")
+    file.write("log.file.location=/change/me.log")
+    file.close()
+
+class Config:
+    __properties = loadProperties()
+
+    @staticmethod
+    def get(name):
+        try:
+            return Config.__properties.get('all', name)
+        except:
+            logging.error("Configuration property %s not found in %s", name, getConfigFileName())
